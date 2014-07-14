9ad5c74adca22186434ab4138c464cec018b3edb
Ross Simmerman <rosssimmerman@Ross-Laptop.local>
2014-07-14 16:00:31 -0400

    Initial Commit

---
+++ RSDreamTeam/RSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  RSAppDelegate.h
+//  RSDreamTeam
+//
+//  Created by Ross Simmerman on 7/14/14.
+//  Copyright (c) 2014 RossSimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
